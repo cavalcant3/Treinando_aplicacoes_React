234d96d07c976f66186c6e3f3b5c7e519d8ec899
Matheus Cavalcante <76076019+cavalcant3@users.noreply.github.com>
2021-07-05 05:38:59 -0300

    fundamentos react

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,41 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 // componentes
 import Primeiro from "./components/Primeiro";
-import ComParametro from "./components/ComParametro";
-import ComFilhos from "./components/ComFilhos";
+import ComParametro from "./basicos/ComParametro";
+import ComFilhos from "./basicos/ComFilhos";
 // import Card from "./components/Card";
 import Card from "./components/layout/Card";
-import Repeticao from "./components/Repeticao";
-import Condicional from "./components/Condicional";
-
+import Repeticao from "./basicos/Repeticao";
+import Condicional from "./basicos/Condicional";
 
 export default (props) => (
   <div className="App">
-    
     <Card titulo="Condicional">
-        <Condicional numero={10} />
-    
-    </Card>   
-      <Card titulo="Repetição">
-        <Repeticao />
-    
+      <Condicional numero={10} />
+    </Card>
+    <Card titulo="Repetição">
+      <Repeticao />
     </Card>
     <Card titulo="Componente com Parametro">
-    <ComFilhos>
-      <ul>
-        <li>ana</li>
-        <li>bia</li>
-        <li>carlos</li>
-        <li>daniel</li>
-      </ul>
-    </ComFilhos>
-    
+      <ComFilhos>
+        <ul>
+          <li>ana</li>
+          <li>bia</li>
+          <li>carlos</li>
+          <li>daniel</li>
+        </ul>
+      </ComFilhos>
     </Card>
     <Card titulo="Exercicio x">
-    <ComParametro 
-      titulo="Componente com Parametro"
-      subtitulo= "esse é o subtitulo"
-    />
-    
+      <ComParametro
+        titulo="Componente com Parametro"
+        subtitulo="esse é o subtitulo"
+      />
     </Card>
-   <Card titulo="Primeiro Componente">
+    <Card titulo="Primeiro Componente">
       <Primeiro />
     </Card>
-
-    {/*  */}
-
-    {/* <App /> */}
-     {/* <Primeiro /> */}
-  </div> 
-
-)
+  </div>
+);
